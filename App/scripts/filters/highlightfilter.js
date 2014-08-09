71be8f3e7cd06924d26a07b295683bd97435a840
Michael Trouw <maggelo@gmail.com>
2014-08-10 01:28:27 +0200

    added filters dir and highlight filter. added tags html css, not working as intended yet.

---
+++ App/scripts/filters/highlightfilter.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc filter
+ * @name pinboredWebkitApp.highlight
+ * @description
+ * # highlight filter
+ * Filter in the pinboredWebkitApp.
+ */
+angular.module('pinboredWebkitApp')
+
+  .filter('highlight', function($sce) {
+    return function(text, phrase) {
+      if (phrase) text = text.replace(new RegExp('('+phrase+')', 'gi'),
+        '<span class="highlight-search">$1</span>')
+
+      return $sce.trustAsHtml(text)
+    }
+  })
