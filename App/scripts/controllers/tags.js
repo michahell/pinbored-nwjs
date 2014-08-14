e28279fc8aa7980ae8a1be2bc2bc1e01b5d3608f
Michael Trouw <maggelo@gmail.com>
2014-08-14 02:10:26 +0200

    wrote get tags rest func. updated readme.

--- App/scripts/controllers/tags.js
+++ App/scripts/controllers/tags.js
@@ -11,12 +11,30 @@ angular.module('pinboredWebkitApp')
   .controller('TagsCtrl', function ($scope, Pinboardservice, Usersessionservice, $location) {
     
     $scope.data = {
-      test : '1'
+      isLoading : true,
+      tags : []
     }
 
     // check if user is logged in on Pinboard
     if (Usersessionservice.isAuthenticated() === false) {
       $location.path("/login");
     }
+
+    function createTags(tagdata) {
+      for (var tag in tagdata) {
+        // console.log(tag, tagdata[tag]);
+        $scope.data.tags.push({tag, tagdata[tag]})
+      }
+    }
+
+    // get all tags
+    console.log("getting ALL tags...");
+    Pinboardservice.getAllTags()
+    .then(function(result) {
+      $scope.data.isLoading = false;
+      $scope.data.tags = createTags(result);
+    }, function(failreason) {
+      console.error('Failed: ' + failreason);
+    });
 
   });
--- App/scripts/services/pinboardservice.js
+++ App/scripts/services/pinboardservice.js
@@ -22,8 +22,28 @@ angular.module('pinboredWebkitApp')
       console.error('error loading restler: ' + error);
     }
 
+    this.handleRestler = function(result, deferred) {
+      if (result instanceof Error) {
+        console.error('Restler: Error: ', result.message);
+        deferred.reject(result.message);
+      } else {
+        var parsedresult;
+        try {
+          parsedresult = JSON.parse(result);
+          deferred.resolve(parsedresult);
+        } catch (exception) {
+          console.error("Pinboardservice: failed to parse request result json: " + exception);
+          console.error(exception);
+          parsedresult = result.toLowerCase();
+          deferred.reject(parsedresult);
+        }
+      }
+    }
+
     this.getUserToken = function(username, password) {
       
+      console.log('pinboardservice: getting user token...')
+
       var deferred = $q.defer();
       // modify url to include username + password,  user:password. See https://pinboard.in/api/ under authentication
       var authstringReplaced = this.authstring.replace('user', username).replace('password', password);
@@ -32,15 +52,7 @@ angular.module('pinboredWebkitApp')
 
       // node restler stuff
       rest.get(request).on('complete', function(result) {
-        deferred.resolve(result);
-        // if (result instanceof Error) {
-        //   console.log('Error:', result.message);
-        //   deferred.reject('Pinboard token request failed: ', result.message);
-        // // this.retry(5000); // try again after 5 sec
-        // } else {
-        //   console.log(result);
-        //   deferred.resolve(data);
-        // }
+        this.handleRestler(result, deferred);
       });
       
       return deferred.promise;
@@ -51,10 +63,11 @@ angular.module('pinboredWebkitApp')
       // check if authenticated
       if(!Usersessionservice.isAuthenticated()) return;
 
-      console.info("requesting " + amount + " bookmarks...");
+      console.log('pinboardservice: getting  ' + amount + ' recent bookmarks...')
 
       var deferred = $q.defer();
       var argAmount, argTags;
+      var self = this;
 
       if(amount === null || amount <= 0)  argAmount = 15;
       else                                argAmount = "&count=" + amount;
@@ -68,23 +81,53 @@ angular.module('pinboredWebkitApp')
 
       // node restler stuff
       rest.get(request).on('complete', function(result) {
-        
-        if (result instanceof Error) {
-          console.error('Restler: Error: ', result.message);
-          deferred.reject(result.message);
-        } else {
-          var parsedresult;
-          try {
-            parsedresult = JSON.parse(result);
-            deferred.resolve(parsedresult);
-          } catch (exception) {
-            console.error("Pinboardservice: failed to parse request result json: " + exception);
-            console.error(exception);
-            parsedresult = result.toLowerCase();
-            deferred.reject(parsedresult);
-          }
-        }
+        self.handleRestler(result, deferred);
+      });
+      
+      return deferred.promise;
+    }
 
+    this.getAllBookmarks = function() {
+
+      // check if authenticated
+      if(!Usersessionservice.isAuthenticated()) return;
+
+      console.log('pinboardservice: getting all bookmarks...')
+
+      var deferred = $q.defer();
+      var self = this;
+
+      // create request
+      var request = this.endpoint + 'posts/all' + this.format + argTags + 
+      this.token.replace('user', Usersessionservice.user).replace('apikey', Usersessionservice.apikey);
+
+      // node restler stuff
+      rest.get(request).on('complete', function(result) {
+        self.handleRestler(result, deferred);
+      });
+      
+      return deferred.promise;
+    }
+
+    this.getAllTags = function() {
+
+      // check if authenticated
+      if(!Usersessionservice.isAuthenticated()) return;
+
+      console.log('pinboardservice: getting all tags...')
+
+      var deferred = $q.defer();
+      var self = this;
+
+      // create request
+      var request = this.endpoint + 'tags/get' + this.format + 
+      this.token.replace('user', Usersessionservice.user).replace('apikey', Usersessionservice.apikey);
+
+      console.info(request);
+      
+      // node restler stuff
+      rest.get(request).on('complete', function(result) {
+        self.handleRestler(result, deferred);
       });
       
       return deferred.promise;
