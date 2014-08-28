2d5849cf5e1d115e02244a00356d25e5f6fa730f
Michael Trouw <maggelo@gmail.com>
2014-08-28 23:46:05 +0200

    removed native .app in build folder.
    Former-commit-id: 112d19c133ee1c9f42fda766473c410978943faf

--- App/scripts/filters/offsetfilter.js
+++ App/scripts/filters/offsetfilter.js
@@ -11,9 +11,11 @@ angular.module('pinboredWebkitApp')
 
   .filter('offset', function() {
     return function(input, start) {
-      
+
       console.log("offset filter invoked.");
-      start = parseInt(start, 10);
-      return input.slice(start);
+      if(input !== undefined && input !== null && input.length > 0) {
+        start = parseInt(start, 10);
+        return input.slice(start);
+      }
     };
   });
