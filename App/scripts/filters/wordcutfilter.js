ecc8d195aba4129771e4f996bff1d8c9a8c44d2f
Michael Trouw <maggelo@gmail.com>
2014-09-09 04:37:46 +0200

    fixed wordwise cutting filter found on s.o.

---
+++ App/scripts/filters/wordcutfilter.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc filter
+ * @name pinboredWebkitApp.wordcut
+ * @description
+ * # wordcut filter
+ * Filter in the pinboredWebkitApp.
+ */
+angular.module('pinboredWebkitApp')
+
+  .filter('wordcut', function () {
+    return function (value, wordwise, max, tail) {
+      if (!value) return '';
+
+      max = parseInt(max, 10);
+      if (!max) return value;
+      if (value.length <= max) return value;
+
+      value = value.substr(0, max);
+      if (wordwise) {
+        var lastspace = value.lastIndexOf(' ');
+        if (lastspace != -1) {
+          value = value.substr(0, lastspace);
+        }
+      }
+
+      return value + (tail || ' …');
+    };
+  });
+
