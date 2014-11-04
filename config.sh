4fc32822b11200e843e5f006dde20a6cc09c2e53
Michael Trouw <maggelo@gmail.com>
2014-11-05 00:13:53 +0100

    updated build.
    Former-commit-id: f8f5a51faeea21b532a8fd605cf4a9910c4efdab

--- config.sh
+++ config.sh
@@ -2,3 +2,17 @@
 
 # increase max open file size for OSX
 # ulimit -S -n 4096
+
+# optional install of required global NPM modules
+echo -n 'Do you wish to install grunt-cli and bower locally ? This requires sudo. if unsure, answer no and install them yourself. the npm postinstall task which is run after this will likely error since grunt will not be installed if you choose no. (y/n) :'
+
+read answer
+
+if  [ $answer == y ]; then
+  sudo npm install -g grunt-cli && sudo npm install -g bower
+elif [ $answer == n ]; then
+  echo 'grunt and bower are both needed to build this project. please install them yourself:'
+  echo 'sudo npm install -g grunt-cli'
+  echo 'sudo npm install -g bower'
+fi
+exit 1
