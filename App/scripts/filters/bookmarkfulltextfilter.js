dfda6c0e20f2d4209d38c35ff8d8f28e9cfdea9f
Michael <maggelo@gmail.com>
2016-02-10 21:44:27 +0100

    fixed bookmarkfulltextfilter.

--- App/scripts/filters/bookmarkfulltextfilter.js
+++ App/scripts/filters/bookmarkfulltextfilter.js
@@ -11,30 +11,22 @@ angular.module('pinboredWebkitApp')
   .filter('bookmarkFulltext', function() {
     return function(bookmarkCollection, word) {
 
-      // console.log('full text filter invoked.');
-      var filteredList = [];
-      var currentBookmark = null;
+      var filteredList = bookmarkCollection;
       var searchFields = ['extended', 'description', 'href', 'tags'];
 
-      if(word !== undefined && word !== '' && word !== ' ') {
-        if(word.length > 0) {
-          for(var j=0; j<bookmarkCollection.length; j++) {
-            currentBookmark = bookmarkCollection[j];
-            // search all searchFields
-            for(var i=0; i<searchFields.length; i++) {
-              var index = currentBookmark[searchFields[i]].toLowerCase().indexOf(word.toLowerCase());
-              if (index > -1) {
-                // set filtered to true and break
-                filteredList.push(currentBookmark);
-                break;
-              }
+      if(word !== undefined && word !== '' && word !== ' ' && word.length > 0) {
+        filteredList = _.filter(bookmarkCollection, function(bm, bmKey) {
+          var filtered = false;
+          _.each(searchFields, function(sf) {
+            if (bm.data[sf].toLowerCase().indexOf(word.toLowerCase()) > -1) {
+              filtered = true;
+              return;
             }
-          }
-        }
-      } else {
-        // there is no word to filter on.
-        return bookmarkCollection;
+          });
+          return filtered;
+        });
       }
+      
       return filteredList;
     };
   });
